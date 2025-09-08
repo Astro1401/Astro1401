f937b2ccbf50ceb9643f89032d0ff286932d66ec
Astro1401 <yadavksanchit@gmail.com>
2025-09-08 19:12:13 +0530

    Time: 0 ms (100%), Space: 19.5 MB (99.43%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(root== NULL) return 0;
-        long long ans = 0;
-        queue<pair<TreeNode*, unsigned long long>> q;
-        q.push({root,0});
-
-        while(!q.empty()){
+        if(!root) 
+            return 0;
+        
+        int ans = 0;
+        queue<pair<TreeNode*, long long>> q;
+        q.push({root, 0});
+        
+        while(!q.empty()) {
             int size = q.size();
-            unsigned long long mmin = q.front().second;
-            unsigned long long first,last;
-
-            for(int i = 0; i< size; i++){
-                unsigned long long cur_id = q.front().second - mmin;
+            long long mmin = q.front().second;  // to make the id starting from zero
+            long long first, last;
+            
+            for(int i = 0; i < size; i++) {
+                long long cur_id = q.front().second - mmin;
                 TreeNode* node = q.front().first;
                 q.pop();
-                if(i==0) first = cur_id;
-                if(i==size-1) last = cur_id;
-                if(node->left) q.push({node->left,cur_id*2+1});
-                if(node->right) q.push({node->right,cur_id*2+2});
+                
+                if(i == 0) first = cur_id;
+                if(i == size - 1) last = cur_id;
+                
+                if(node->left) 
+                    q.push({node->left, cur_id * 2 + 1});
+                if(node->right) 
+                    q.push({node->right, cur_id * 2 + 2});
             }
-            ans = max(ans,(long long)(last-first+1));
+            
+            ans = max(ans, (int)(last - first + 1));
         }
-        return (int)ans;
+        
+        return ans;
     }
 };
