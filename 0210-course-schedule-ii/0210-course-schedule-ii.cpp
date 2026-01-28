be9aa7ce7c4192dbfb6e0d7826b7c107260e5568
Astro1401 <yadavksanchit@gmail.com>
2026-01-28 20:07:37 +0530

    Time: 2 ms (76.83%), Space: 18.3 MB (57.92%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,48 +1,40 @@
 class Solution {
-private:
-    bool dfs(int node, vector<int> adj[], int vis[], int pathVis[], vector<int>& topo) {
-        vis[node] = 1;
-        pathVis[node] = 1;
-
-        for (auto it : adj[node]) {
-            if (!vis[it]) {
-                if (dfs(it, adj, vis, pathVis, topo))
-                    return true;
-            }
-            else if (pathVis[it]) {
-                return true;
-            }
-        }
-
-        pathVis[node] = 0;
-        topo.push_back(node);
-        return false;
-    }
-
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];
+         vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
 
-        // build graph: prereq → course
-        for (auto &p : prerequisites) {
-            adj[p[1]].push_back(p[0]);
+        // Build graph
+        for (auto &it : prerequisites) {
+            adj[it[1]].push_back(it[0]);
+            indegree[it[0]]++;
         }
 
-        int vis[numCourses];
-        int pathVis[numCourses];
-        memset(vis, 0, sizeof(vis));
-        memset(pathVis, 0, sizeof(pathVis));
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
 
         vector<int> topo;
 
-        for (int i = 0; i < numCourses; i++) {
-            if (!vis[i]) {
-                if (dfs(i, adj, vis, pathVis, topo))
-                    return {}; // cycle found
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for (auto it : adj[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.push(it);
+                }
             }
         }
+if (topo.size() != numCourses)
+    return {};
 
-        reverse(topo.begin(), topo.end());
-        return topo;
+return topo;
+
     }
 };
