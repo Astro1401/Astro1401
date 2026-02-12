ed3075bf096d20f1dc3eb44f762455b415bec3f0
Astro1401 <yadavksanchit@gmail.com>
2026-02-12 11:34:53 +0530

    Time: 42 ms (96.95%), Space: 25.5 MB (52.24%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     int longestBalanced(string s) {
         int n = s.size();
-        int res =0;
-        vector<int> cnt(26);
-        
-        for(int i = 0; i<n; i++){
-           fill(cnt.begin(),cnt.end(),0);
-            for(int j = i; j<n; j++){
-                bool flag = true;
-                int c = s[j]-'a';
-                cnt[c]++;
-                for(auto x : cnt){
-                    if(x>0 && x!=cnt[c]){
-                        flag = false;
-                        break;
-                    }
-                }
+        int res = 0;
 
-                if(flag){
-                    res = max(res,j-i+1);
+        for (int i = 0; i < n; i++) {
+            vector<int> cnt(26, 0);
+            int distinct = 0, maxFreq = 0;
+
+            for (int j = i; j < n; j++) {
+                int c = s[j] - 'a';
+
+                if (cnt[c] == 0) distinct++;
+                cnt[c]++;
+                maxFreq = max(maxFreq, cnt[c]);
+
+                int len = j - i + 1;
+
+                if (maxFreq * distinct == len) {
+                    res = max(res, len);
                 }
             }
         }
