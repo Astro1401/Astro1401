50418534df9e68322b23116d6a228d4b320e1e87
Astro1401 <yadavksanchit@gmail.com>
2026-02-13 14:29:26 +0530

    Time: 179 ms (79.44%), Space: 115.5 MB (69.19%) - LeetHub

---
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -0,0 +1,113 @@
+class DisjointSet { 
+
+public:
+     vector<int> rankv, parent, sizev;
+    DisjointSet(int n) {
+        rankv.resize(n + 1, 0);
+        parent.resize(n + 1);
+        sizev.resize(n + 1, 1);
+
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v) return;
+
+        if (rankv[ulp_u] < rankv[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rankv[ulp_v] < rankv[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rankv[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v) return;
+
+        if (sizev[ulp_u] < sizev[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            sizev[ulp_v] += sizev[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            sizev[ulp_u] += sizev[ulp_v];
+        }
+    }
+};
+
+class Solution {
+private: 
+    bool isvalid(int newr , int newc , int n){
+        return newr>=0 && newr<n && newc>=0 && newc<n;
+    }
+public:
+    int largestIsland(vector<vector<int>>& grid) {
+        int n = grid.size();
+        DisjointSet ds(n*n);
+
+        for(int row = 0; row< n; row++){
+            for(int col = 0; col<n; col++){
+                if(grid[row][col]==0)continue;
+                int dr[]={-1,0,1,0};
+                int dc[]={0,-1,0,1};
+
+                for(int ind = 0; ind<4; ind++){
+                    int newr = row + dr[ind];
+                    int newc = col + dc[ind];
+            
+                if(isvalid(newr,newc,n) && grid[newr][newc]==1){
+                    int nodeno = row*n+col;
+                    int adjnodeno = newr*n+newc;
+                    ds.unionBySize(nodeno,adjnodeno);
+                }
+            }
+        }
+    }
+        int mx =0;
+        for(int row = 0; row<n; row++){
+            for(int col = 0; col<n; col++){
+                if(grid[row][col]==1) continue;
+                  int dr[]={-1,0,1,0};
+                  int dc[]={0,-1,0,1};
+                set<int> components;
+                for(int ind = 0; ind<4; ind++){
+                    int newr = row + dr[ind];
+                    int newc = col + dc[ind];
+                    if(isvalid(newr,newc,n)){
+                        if(grid[newr][newc]==1){
+                            components.insert(ds.findUPar(newr*n+newc));
+                        }
+                    }
+             }
+             int size = 0;
+             for(auto it:components){
+                size += ds.sizev[it];
+             }
+             mx = max(mx,size+1);
+          }
+        }
+
+        for(int cellno = 0; cellno<n*n; cellno++){
+            mx = max(mx,ds.sizev[ds.findUPar(cellno)]);
+        }
+        return mx;
+    }
+};
