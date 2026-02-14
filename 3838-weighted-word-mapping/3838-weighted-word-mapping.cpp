b63dfc1542427761095491a907951456afdbfcf0
Astro1401 <yadavksanchit@gmail.com>
2026-02-15 01:50:40 +0530

    Time: 0 ms (100%), Space: 43.5 MB (0%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+
+        string ans = "";
+
+        for(string word : words){
+            int sum = 0;
+
+            for(char c : word){
+                sum += weights[c-'a'];
+            }
+
+            sum = sum%26;
+
+            char map = 'z'-sum;
+
+            ans += map;
+        }
+
+        return ans;
+    }
+};
