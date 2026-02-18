c967a16e934ff9a0bcc74306904c2a395192e7da
Astro1401 <yadavksanchit@gmail.com>
2026-02-18 11:08:12 +0530

    Time: 2 ms (22.38%), Space: 9.7 MB (41.71%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> ans;
+        
+        for(int hour = 0; hour < 12; hour++) {
+            for(int minute = 0; minute < 60; minute++) {
+                
+                // Count total set bits
+                if(__builtin_popcount(hour) + __builtin_popcount(minute) == turnedOn) {
+                    
+                    string time = to_string(hour) + ":";
+                    
+                    // Add leading zero if needed
+                    if(minute < 10)
+                        time += "0";
+                    
+                    time += to_string(minute);
+                    
+                    ans.push_back(time);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
