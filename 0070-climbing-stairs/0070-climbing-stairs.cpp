f7130eedc34b1431cc7a72bd84273a7f58fb0ce3
Astro1401 <yadavksanchit@gmail.com>
2026-02-26 19:07:51 +0530

    Time: 0 ms (100%), Space: 8.6 MB (17.8%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int func(int ind, vector<int> &dp){
-        if(ind==0) return 1;
-        if(ind==1) return 1;
-        if(dp[ind] != -1) return dp[ind];
-        int left = func(ind-1,dp);
-        int right = func(ind-2,dp);
-
-        return dp[ind] = left+right;
-    }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return func(n, dp);;
+        vector<int> dp(n + 1);
+
+        // base cases
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
     }
 };
