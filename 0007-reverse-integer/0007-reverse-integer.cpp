ad8cd253638d5a3b92d52bd783c475b5b8147d0d
Astro1401 <yadavksanchit@gmail.com>
2026-03-03 11:01:38 +0530

    Time: 0 ms (100%), Space: 8.6 MB (20.17%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int revno = 0;
+
+        while(x!=0){
+            int ld = x % 10;
+            if(revno > INT_MAX / 10 || revno < INT_MIN / 10) return 0;
+            revno = (revno*10) + ld;
+            x=x/10;
+        }
+
+        return revno;
+    }
+};
