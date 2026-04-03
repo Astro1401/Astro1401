6995e46dea7d2adf5a34fea64263e8e68b10d2af
Astro1401 <yadavksanchit@gmail.com>
2026-04-04 00:06:23 +0530

    Time: 9 ms (26.67%), Space: 24.9 MB (17.85%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -5,7 +5,7 @@ public:
 
         vector<vector<int>> dp(n+1,vector<int>(2,0));
 
-        dp[n][0] = dp[n][1] = 0;
+        //dp[n][0] = dp[n][1] = 0;
 
         for(int i = n-1; i>=0; i--){
             for(int j = 0; j<=1; j++){
