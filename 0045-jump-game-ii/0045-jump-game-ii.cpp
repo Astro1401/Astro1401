385453b277d08774abcee113b3938f38d672e632
Astro1401 <yadavksanchit@gmail.com>
2026-04-06 23:12:03 +0530

    Time: 0 ms (100%), Space: 20.6 MB (32.67%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+       
+        int l = 0, r = 0;
+        int jumps = 0;
+       
+       while(r<n-1){
+        int farest = 0;
+
+        for(int i = l; i<=r; i++){
+            farest = max(farest,nums[i]+i);
+        }
+            l = r + 1 ;
+            r = farest;
+            jumps++;
+       }
+        return jumps;
+    }
+};
