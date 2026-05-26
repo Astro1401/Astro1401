033339c73beb07636665cfd903c55256dc2d1c29
Astro1401 <yadavksanchit@gmail.com>
2026-05-26 15:20:28 +0530

    Time: 133 ms (13.26%), Space: 25.8 MB (15.75%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        unordered_map<int,int> mp;
+        int count = 0;
+        for(int i = 0; i< word.size(); i++){
+            if(word[i] >= 'a' && word[i] <= 'z'){
+                mp[word[i]]++;
+            }
+        }
+                      
+        for(int i = 0; i<word.size(); i++){
+            if(word[i] >= 'A' && word[i] <= 'Z'){
+                mp[word[i]]++;
+                char ch = tolower(word[i]);
+                if(mp[ch]>0 && mp[word[i]]<=1){
+                     mp[ch] = -1;
+                     count++;
+                }
+            }
+
+            if(mp[word[i]] == -1) {
+                count--;
+                mp[word[i]] = 0;
+            }
+        }
+        return count;
+    }
+};
