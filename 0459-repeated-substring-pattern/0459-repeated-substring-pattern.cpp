ed813ad251c2de55a4b0fb7bc98c0a0193f89e52
Astro1401 <yadavksanchit@gmail.com>
2026-07-13 00:58:21 +0530

    Time: 18 ms (44.18%), Space: 47.7 MB (37.04%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int n = s.length();
+        for(int l = n/2; l >= 1; l--){
+            string ram = "";
+            if(n % l == 0){
+                int times = n/l;
+                string ss = s.substr(0,l);
+                while(times--){
+                    ram += ss;
+                }
+
+                if(ram == s) return true;
+            }
+        }
+         return false;
+    }
+};
