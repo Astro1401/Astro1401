7b41fcc71ed864c8b21c4fd1319c4b77a5fe0cfe
Astro1401 <yadavksanchit@gmail.com>
2026-07-13 21:38:35 +0530

    Time: 0 ms (100%), Space: 8.4 MB (18.24%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        queue<int> q;
+
+        for(int i = 1; i<=8; i++){
+            q.push(i);
+        }
+
+        vector<int> result;
+
+        while(!q.empty()){
+            int temp = q.front();
+            q.pop();
+
+            if(temp >= low && temp <= high){
+                result.push_back(temp);
+            }
+
+            int last = temp%10;
+            if(last + 1 <= 9){
+                q.push(temp * 10 + (last + 1));
+            }
+        }
+        return result;
+    }
+};
