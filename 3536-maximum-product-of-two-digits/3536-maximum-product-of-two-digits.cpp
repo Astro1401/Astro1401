665e289030ae6bf4336868be08d0c156bb40346c
Astro1401 <yadavksanchit@gmail.com>
2026-07-25 20:27:28 +0530

    Time: 0 ms (100%), Space: 8.7 MB (59.34%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        int temp = n;
-        int firstmax = INT_MIN;
-        int tmep2 = n;
-        unordered_map<int,int> mp;
-        
-        while(tmep2>0){
-            int digit = tmep2%10;
-            mp[digit]++;
-            tmep2 =  tmep2/10;
+        int max1 = 0, max2 = 0;
+
+        for (; n; n /= 10) {
+            int d = n % 10;
+            if (d >= max1) {
+                max2 = max1;
+                max1 = d;
+            } else if (d > max2) 
+                max2 = d;
         }
 
-        while(temp>0){
-            int digit = temp%10;
-            firstmax = max(digit,firstmax);
-            temp = temp/10;
-        }
-
-        int secondmax = INT_MIN;
-        while(n>0){
-            int digit = n%10;
-            if(digit > secondmax && digit == firstmax && mp[digit] > 1){
-                secondmax = digit;
-            }
-            else if(digit>secondmax && digit < firstmax ){
-                secondmax = digit;
-            }
-            n = n/10;
-        }
-       cout << firstmax;
-       cout << secondmax;
-        return firstmax*secondmax;
+        return max1 * max2;
     }
 };
