9a6487ed826968bffea0dd74e07ae8728380de54
Astro1401 <yadavksanchit@gmail.com>
2026-07-31 11:53:51 +0530

    Time: 7 ms (21.51%), Space: 11.9 MB (49.69%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<int,int> mp;
+        for(int i = 0; i<magazine.length(); i++){
+           mp[magazine[i]]++;
+        }
+
+        for(int i = 0; i<ransomNote.length(); i++){
+            if(mp[ransomNote[i]] == 0)
+                return false;
+
+            mp[ransomNote[i]]--;
+        }
+        return true;
+    }
+};
