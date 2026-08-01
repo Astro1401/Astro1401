c38609413376ff390ccfbe70ba3370cfbac386f3
Astro1401 <yadavksanchit@gmail.com>
2026-08-01 17:11:36 +0530

    Time: 0 ms (100%), Space: 10.2 MB (32.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<int> &nums, vector<vector<int>>& dp){
-        if(i>j) return 0;
-        if(i==j) return nums[i];
-        if(dp[i][j] != -1) return dp[i][j];
-        int takei = nums[i] + min(solve(i+2,j,nums,dp),solve(i+1,j-1,nums,dp));
+    int solve(int i, int j, vector<int>& nums, vector<vector<int>>& dp) {
+        if (i == j)
+            return nums[i];
 
-        int takej = nums[j] + min(solve(i,j-2,nums,dp), solve(i+1,j-1,nums,dp));
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
-        return dp[i][j] = max(takei,takej);
+        int takeLeft = nums[i] - solve(i + 1, j, nums, dp);
+        int takeRight = nums[j] - solve(i, j - 1, nums, dp);
+
+        return dp[i][j] = max(takeLeft, takeRight);
     }
+
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        int sum = 0;
-        
+
         vector<vector<int>> dp(n, vector<int>(n, -1));
-        
-        for(auto it : nums){
-          sum += it;
-        }
 
-        int p1score = solve(0,n-1,nums,dp);
-        int p2score = sum - p1score;
-
-        return p1score>=p2score;
+        return solve(0, n - 1, nums, dp) >= 0;
     }
 };
