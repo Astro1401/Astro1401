fce33ab198bd769dddf04f7a7e384cc2544b90c1
Astro1401 <yadavksanchit@gmail.com>
2026-08-02 06:35:29 +0530

    Time: 0 ms (100%), Space: 11.3 MB (94.25%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -21,7 +21,7 @@ public:
          
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
-                if(i>=0 && j>=0 && obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
                 else if(i==0 && j==0) dp[i][j] = 1;
                 else{
                    int up = 0;
