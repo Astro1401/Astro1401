3f02d4faac08c0c921533315e43f4b272b611070
Astro1401 <yadavksanchit@gmail.com>
2026-08-02 06:40:47 +0530

    Time: 0 ms (100%), Space: 16.4 MB (43.01%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -10,12 +10,13 @@ public:
             for(int j = 0; j<n; j++){
                 if(i==0 && j==0) dp[i][j] = grid[i][j];
                 else{
-                      int up = grid[i][j];
-                      if(i>0) up += dp[i-1][j];
+                      int up = 0;
+                      int left = 0;
+
+                      if(i>0) up = grid[i][j] + dp[i-1][j];
                       else up += 1e9;
 
-                      int left = grid[i][j];
-                      if(j>0) left += dp[i][j-1];
+                      if(j>0) left = grid[i][j] + dp[i][j-1];
                       else left += 1e9;
 
                       dp[i][j] = min(up,left);
